0a1d3b588c5aa45c4014bb1d48c32077667b0379
Md. Jahid Hasan <mdjahidhasanf92@gmail.com>
2021-07-31 22:49:02 +0600

    UI layer

---
+++ UI/model.js
@@ -0,0 +1,56 @@
+const open = document.getElementById('open');
+const model_container = document.getElementById('model_container');
+const close = document.getElementById('close');
+
+open.addEventListener('click', () => {
+  model_container.classList.add('show');
+});
+close.addEventListener('click', () => {
+  model_container.classList.remove('show');
+});
+
+const open1 = document.getElementById('open1');
+const model_container1 = document.getElementById('model_container1');
+const close1 = document.getElementById('close1');
+
+open1.addEventListener('click', () => {
+  model_container1.classList.add('show');
+});
+close1.addEventListener('click', () => {
+  model_container1.classList.remove('show');
+});
+
+const open2 = document.getElementById('open2');
+const model_container2 = document.getElementById('model_container2');
+const close2 = document.getElementById('close2');
+
+open2.addEventListener('click', () => {
+  model_container2.classList.add('show');
+});
+close2.addEventListener('click', () => {
+  model_container2.classList.remove('show');
+});
+
+const open3 = document.getElementById('open3');
+const model_container3 = document.getElementById('model_container3');
+const close3 = document.getElementById('close3');
+
+open3.addEventListener('click', () => {
+  model_container3.classList.add('show');
+});
+close3.addEventListener('click', () => {
+  model_container3.classList.remove('show');
+});
+
+const panels = document.querySelectorAll('.code-done-by')
+panels.forEach(panel => {
+  panel.addEventListener('click', () => {
+    removeActiveClass();
+    panel.classList.add('active')
+  })
+})
+function removeActiveClass() {
+  panels.forEach(panel => {
+    panel.classList.remove('active');
+  })
+}
